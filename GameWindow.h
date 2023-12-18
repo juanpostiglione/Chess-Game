052425c149e226af71076db0aceff114ddc1239e
Juan Postiglione <juan.postiglione@ufl.edu>
2023-12-18 00:20:56 -0400

    GameWindow.h

--- GameWindow.h
+++ GameWindow.h
@@ -1,1 +1,21 @@
+#include<iostream>
+#include <SFML/Graphics.hpp>
+#pragma once
 
+using namespace std;
+
+class Board;
+
+class GameWindow{
+private:
+    int width;
+    int height;
+    sf::Sprite selectedPiece;
+
+public:
+    /// DEFAULT CONSTRUCTOR ///
+    GameWindow();
+
+    /// FUNCTION TO DISPLAY GAME WINDOW ///
+    void GameWindowDisplay(Board& chessBoard);
+};
