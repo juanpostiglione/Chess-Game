b5676eba25cb534c93b91fd57f5715d5184980cd
Juan Postiglione <juan.postiglione@ufl.edu>
2023-12-21 01:48:23 -0400

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include"WelcomeWindow.h"
-
+#include "Piece.h"
 int main()
 {
     Board chessBoard;
+    Pawn pawnPieces;
+    Rook rookPieces;
+    Knight knightPieces;
+    Bishop bishopPieces;
+    King kingPieces;
+    Queen queenPieces;
     WelcomeWindow window1;
-    window1.welcomeWindowDisplay(chessBoard);
+    window1.welcomeWindowDisplay(chessBoard, pawnPieces, rookPieces, knightPieces, bishopPieces, kingPieces,queenPieces);
     return 0;
 }
