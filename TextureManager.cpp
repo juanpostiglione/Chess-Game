ec3044fbacdcc4e2de9884105d465f4352f54385
Juan Postiglione <juan.postiglione@ufl.edu>
2023-12-23 23:14:33 -0400

    TextureManager.cpp

--- TextureManager.cpp
+++ TextureManager.cpp
@@ -7,14 +7,14 @@ unordered_map<string, sf::Texture> TextureManager::textures;
 sf::Texture& TextureManager::getTexture(std::string textureName) {
     auto result = textures.find(textureName);
     if(result == textures.end()) {
-        // Texture does not already exist in the map, go get it!
+        /// TEXTURE DOES NOT ALREADY EXIST IN THE MAP, GO GET IT ///
         sf::Texture newTexture;
         newTexture.loadFromFile("images/" + textureName + ".png");
         textures[textureName] = newTexture;
         return textures[textureName];
     }
     else {
-        // Texture already exists, return it!
+        /// TEXTURE ALREADY EXIST ///
         return result->second;
     }
 }
