3c9f507204c61fb5104c81aa03f7cfafafe708c3
Juan Postiglione <juan.postiglione@ufl.edu>
2023-12-28 15:53:20 -0400

    Piece.h

--- Piece.h
+++ Piece.h
@@ -5,14 +5,13 @@ using namespace std;
 /// BASE CLASS OF PIECES ///
 class Piece{
 public:
-    /// FUNCTION TO DRAW THE PIECE ///
-    virtual void draw(sf::RenderWindow& window, bool pieceSelected, bool pieceReleased,
+    /// FUNCTION TO MOVE AND DRAW THE PIECE ///
+    virtual void move(sf::RenderWindow& window, bool pieceSelected, bool pieceReleased,
               vector<sf::Sprite>& piece, const sf::Vector2i& mouse, int positionX, int positionY) const = 0;
 
-    /// FUNCTION TO MOVE THE PIECE ///
-    virtual void move(sf::Event& event, sf::RenderWindow& gameWindow,
+    /// FUNCTION TO SET THE STATE OF THE PIECE ///
+    virtual void pieceState(sf::Event& event, sf::RenderWindow& gameWindow,
                       vector<sf::Sprite>& pieces, bool& piece_selected, bool& piece_released) const = 0;
-
 
 };
 
@@ -20,14 +19,12 @@ public:
 class Pawn : public Piece /// Inherit from Piece Class
 {
 public:
-
-    /// DRAW PAWNS ///
-    void draw(sf::RenderWindow& window, bool pawnSelected, bool pawnReleased,
-              vector<sf::Sprite>& pawns, const sf::Vector2i& mouse, int positionX, int positionY) const;
-
-    /// MOVE PAWNS ///
-    void move(sf::Event& event, sf::RenderWindow& gameWindow,
-              vector<sf::Sprite>& pawns, bool& pawn_selected, bool& pawn_released) const;
+    
+    void move(sf::RenderWindow& window, bool pawnSelected, bool pawnReleased,
+              vector<sf::Sprite>& pawns, const sf::Vector2i& mouse, int positionX, int positionY) const override;
+    
+    void pieceState(sf::Event& event, sf::RenderWindow& gameWindow,
+              vector<sf::Sprite>& pawns, bool& pawn_selected, bool& pawn_released) const override;
 
 };
 
@@ -35,55 +32,47 @@ public:
 class Rook : public Piece /// Inherit from Piece Class
 {
 public:
-
-    /// DRAW ROOKS ///
-    void draw(sf::RenderWindow& window, bool rookSelected, bool rookReleased,
-              vector<sf::Sprite>& rooks, const sf::Vector2i& mouse, int positionX, int positionY) const;
-
-    /// MOVE ROOKS ///
-    void move(sf::Event& event, sf::RenderWindow& gameWindow,
-              vector<sf::Sprite>& rooks, bool& rook_selected, bool& rook_released) const;
+    
+    void move(sf::RenderWindow& window, bool rookSelected, bool rookReleased,
+              vector<sf::Sprite>& rooks, const sf::Vector2i& mouse, int positionX, int positionY) const override;
+    
+    void pieceState(sf::Event& event, sf::RenderWindow& gameWindow,
+              vector<sf::Sprite>& rooks, bool& rook_selected, bool& rook_released) const override;
 };
 
 /// CLASS FOR KNIGHT ///
 class Knight : public Piece /// Inherit from Piece Class
 {
 public:
-
-    /// DRAW KNIGHTS ///
-    void draw(sf::RenderWindow& window, bool knightSelected, bool knightReleased,
-              vector<sf::Sprite>& knights, const sf::Vector2i& mouse, int positionX, int positionY) const;
-
-    /// MOVE KNIGHTS ///
-    void move(sf::Event& event, sf::RenderWindow& gameWindow,
-              vector<sf::Sprite>& knights, bool& knights_selected, bool& knights_released) const;
+    
+    void move(sf::RenderWindow& window, bool knightSelected, bool knightReleased,
+              vector<sf::Sprite>& knights, const sf::Vector2i& mouse, int positionX, int positionY) const override;
+    
+    void pieceState(sf::Event& event, sf::RenderWindow& gameWindow,
+              vector<sf::Sprite>& knights, bool& knights_selected, bool& knights_released) const override;
 };
 
 /// CLASS FOR BISHOP ///
 class Bishop : public Piece /// Inherit from Piece Class
 {
 public:
-
-    /// DRAW BISHOPS ///
-    void draw(sf::RenderWindow& window, bool bishopSelected, bool bishopReleased,
-              vector<sf::Sprite>& bishops, const sf::Vector2i& mouse, int positionX, int positionY) const;
-
-    /// MOVE BISHOPS ///
-    void move(sf::Event& event, sf::RenderWindow& gameWindow,
-              vector<sf::Sprite>& bishops, bool& bishop_selected, bool& bishop_released) const;
+    
+    void move(sf::RenderWindow& window, bool bishopSelected, bool bishopReleased,
+              vector<sf::Sprite>& bishops, const sf::Vector2i& mouse, int positionX, int positionY) const override;
+    
+    void pieceState(sf::Event& event, sf::RenderWindow& gameWindow,
+              vector<sf::Sprite>& bishops, bool& bishop_selected, bool& bishop_released) const override;
 };
 
 /// CLASS FOR KING ///
 class King
 {
 public:
-
-    /// DRAW KINGS ///
-    void draw(sf::RenderWindow& window, bool kingSelected, bool kingReleased,
+    
+    void move(sf::RenderWindow& window, bool kingSelected, bool kingReleased,
               sf::Sprite& kings, const sf::Vector2i& mouse, int positionX, int positionY) const;
-
-    /// MOVE KINGS ///
-    void move(sf::Event& event, sf::RenderWindow& gameWindow,
+    
+    void pieceState(sf::Event& event, sf::RenderWindow& gameWindow,
               sf::Sprite& kings, bool& king_selected, bool& king_released) const;
 
 };
@@ -92,13 +81,11 @@ public:
 class Queen
 {
 public:
-
-    /// DRAW QUEENS ///
-    void draw(sf::RenderWindow& window, bool queenSelected, bool queenReleased,
+    
+    void move(sf::RenderWindow& window, bool queenSelected, bool queenReleased,
               sf::Sprite& queens, const sf::Vector2i& mouse, int positionX, int positionY) const;
-
-    /// MOVE QUEENS ///
-    void move(sf::Event& event, sf::RenderWindow& gameWindow,
+    
+    void pieceState(sf::Event& event, sf::RenderWindow& gameWindow,
               sf::Sprite& queens, bool& queen_selected, bool& queen_released) const;
 
 };
