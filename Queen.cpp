2bf25f4e29d8e6687d2ca1ec2e7da711528853dd
Juan Postiglione <juan.postiglione@ufl.edu>
2024-01-02 17:41:43 -0400

    Queen.cpp

--- Queen.cpp
+++ Queen.cpp
@@ -1,13 +1,13 @@
 #include "Queen.h"
 
 /// FUNCTION TO DRAW QUEENS ///
-void Queen::move(sf::RenderWindow& window, bool kingSelected, bool kingReleased,
+void Queen::move(sf::RenderWindow& window, bool queenSelected, bool queenReleased,
                 sf::Sprite& queens, const sf::Vector2i& mouse, int positionX, int positionY)
 {
     /// IF KING IS SELECTED, IT CAN BE SEEN WHILE IS BEING MOVED ///
     for (int i = 0; i < 1; i++)
     {
-        if (kingSelected && queens.getGlobalBounds().contains(window.mapPixelToCoords(mouse)))
+        if (queenSelected && queens.getGlobalBounds().contains(window.mapPixelToCoords(mouse)))
         {
             queens.setPosition(static_cast<float>(mouse.x - 30), static_cast<float>(mouse.y - 30));
             window.draw(queens);
@@ -24,7 +24,7 @@ void Queen::move(sf::RenderWindow& window, bool kingSelected, bool kingReleased,
         queens.setPosition(static_cast<float>(snappedX), static_cast<float>(snappedY));
 
         /// DRAW KINGS FROM THE BEGINNING ///
-        if (!kingReleased && !kingSelected)
+        if (!queenReleased && !queenSelected)
         {
             queens.setPosition(static_cast<float>(positionX), static_cast<float>(positionY));
         }
